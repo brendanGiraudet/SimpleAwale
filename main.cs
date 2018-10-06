704e4f755b8a38d966f195fba8eea72aa6a6ff21
brendanGiraudet <brendan.giraudet1@gmail.com>
2018-10-06 18:19:42 +0200

    display list

--- main.cs
+++ main.cs
@@ -13,8 +13,35 @@ class Solution
 {
     static void Main(string[] args)
     {
+        // opponent
         string opBowls = Console.ReadLine();
+        var opList = opBowls.Split(' ').ToList();
+        // me
         string myBowls = Console.ReadLine();
+        var myList = myBowls.Split(' ').ToList();
+        // index of choosen bowl
         int num = int.Parse(Console.ReadLine());
+        // display opponent bowls
+        DisplayBowls(opList);
+        // display my bowls
+        DisplayBowls(myList);
+    }
+
+    static void DisplayBowls(List<string> bowlList)
+    {
+        var line = "";
+        bowlList.ForEach( b => {
+            // if last bowl => reserve
+            if(bowlList.IndexOf(b) == bowlList.Count -1)
+            {
+                line += " [" + b + "]";
+            }
+            else
+            {
+                line += " " + b;
+            }
+            System.Console.Error.WriteLine(b);
+        });
+        System.Console.WriteLine(line.Trim());
     }
 }
