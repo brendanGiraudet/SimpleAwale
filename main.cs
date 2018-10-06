2b58f970bc05935385eb609e6b4f79d4d137eb7d
brendanGiraudet <brendan.giraudet1@gmail.com>
2018-10-06 18:56:03 +0200

    finish

--- main.cs
+++ main.cs
@@ -15,24 +15,52 @@ class Solution
     {
         // opponent
         string opBowls = Console.ReadLine();
-        var opList = opBowls.Split(' ').ToList();
+        var opList = opBowls.Split(' ').ToList().Select(val => Convert.ToInt32(val)).ToList();
         // me
         string myBowls = Console.ReadLine();
-        var myList = myBowls.Split(' ').ToList();
+        var myList = myBowls.Split(' ').ToList().Select(val => Convert.ToInt32(val)).ToList();
         // index of choosen bowl
         int num = int.Parse(Console.ReadLine());
+        // get grains
+        int nbGrains = myList[num];
+        myList[num] = 0;
+        num++;
+        var needReplay = false;
+        do
+        {
+            // my foreach
+            for (int i = num; i < 7 && nbGrains != 0; i++)
+            {
+                nbGrains--;
+                myList[i]++;
+                needReplay = (i + 1 == 7 && nbGrains == 0);
+            }
+            num = 0;
+            // opponent foreach
+            for (int j = 0; j < 6 && nbGrains != 0; j++)
+            {
+                nbGrains--;
+                opList[j]++;
+            }
+        } while (nbGrains != 0);
         // display opponent bowls
         DisplayBowls(opList);
         // display my bowls
         DisplayBowls(myList);
+        // replay
+        if(needReplay)
+        {
+            System.Console.WriteLine("REPLAY");
+        }
     }
 
-    static void DisplayBowls(List<string> bowlList)
+    static void DisplayBowls(List<int> bowlList)
     {
         var line = "";
+        int index = 0;
         bowlList.ForEach( b => {
             // if last bowl => reserve
-            if(bowlList.IndexOf(b) == bowlList.Count -1)
+            if(index == bowlList.Count -1)
             {
                 line += " [" + b + "]";
             }
@@ -40,7 +68,7 @@ class Solution
             {
                 line += " " + b;
             }
-            System.Console.Error.WriteLine(b);
+            index++;
         });
         System.Console.WriteLine(line.Trim());
     }
