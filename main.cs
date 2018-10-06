b89f8b561c1e52c26191d1049c5ccd02d8468e76
brendanGiraudet <brendan.giraudet1@gmail.com>
2018-10-06 17:46:48 +0200

    first commit

---
+++ main.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution
+{
+    static void Main(string[] args)
+    {
+        string opBowls = Console.ReadLine();
+        string myBowls = Console.ReadLine();
+        int num = int.Parse(Console.ReadLine());
+    }
+}
